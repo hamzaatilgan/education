fa490828519c4978e80e907cd0d4a705e18c691b
Hamza ATILGAN <122400602+hamzaatilgan@users.noreply.github.com>
2024-10-19 00:32:46 +0300

    [fix] content update
    let - değiştirilebilir bir variable tanımı
    const - değiştirilemez variable tanımı anlamına geldiği öğrenildi JS ders 1 tamamlandı.

--- 30DaysOfJS/Day1/introduction.js
+++ 30DaysOfJS/Day1/introduction.js
@@ -1,1 +1,9 @@
-console.log('Bu yazı introduction.js dosyasından geldi bir external script örneği.' )
+
+
+let youtubechannel = 'Hamzanın Kanalı'
+let channelage = 1
+let channelactive = true
+let secondchannel = null
+let channelmoney = undefined
+
+console.log(youtubechannel, channelage, channelactive, secondchannel, channelmoney)
